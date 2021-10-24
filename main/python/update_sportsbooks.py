57646bc8fd5072c1583111565ae70c93bba60c43
Ben <ben@wizehire.com>
2021-10-24 14:55:34 -0500

    remove redundancies for sportsbook_id updating script

--- main/python/update_sportsbooks.py
+++ main/python/update_sportsbooks.py
@@ -1,10 +1,10 @@
 import datetime
 
-from pysbr import Sportsbook
+from pysbr import *
 
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='',
+cnx = mysql.connector.connect(user='ben', password='',
                               host='127.0.0.1',
                               database='odds')
 cursor = cnx.cursor()
@@ -16,24 +16,20 @@ sportsbook_keys_json = sb.sportsbook_config()['sportsbooks']
 inserts = []
 for book in sportsbook_keys_json:
     write = True
-    # print(inserts)
+    print(book)
     if book['sportsbook id']:
-        print(book['sportsbook id'])
         check_sportsbook_query = "SELECT * FROM sportsbooks WHERE sportsbook_id = %s"
         cursor.execute(check_sportsbook_query, (book['sportsbook id'],))
         for (sportsbook_id, system_id, name, alias) in cursor:
-            print(sportsbook_id, system_id, name, alias)
-            if sportsbook_id == book['sportsbook id']:
-                print(sportsbook_id, book['sportsbook id'])
+            if sportsbook_id == book['sportsbook id'] and system_id == book['system sportsbook id']:
                 write = False
-        # if write:
-        #     print("WRITING: ", book['sportsbook id'])
-        #     add_sportsbook_query = "INSERT INTO `sportsbooks` (`sportsbook_id`, `system_id`, `name`, `alias`) VALUES (%s,%s,%s,%s)"
-        #     cursor = cnx.cursor()
-        #     cursor.execute(add_sportsbook_query, (book['sportsbook id'], book['system sportsbook id'], book['name'], book['alias']))
-        #     inserts += [(book['sportsbook id'], book['system sportsbook id'], book['name'], book['alias'])]
+        if write:
+            print("WRITING: ", book['sportsbook id'])
+            add_sportsbook_query = "INSERT INTO `sportsbooks` (`sportsbook_id`, `system_id`, `name`, `alias`) VALUES (%s,%s,%s,%s)"
+            inserts += [(book['sportsbook id'], book['system sportsbook id'], book['name'], book['alias'])]
 
-# add_sportsbook_query = "INSERT INTO `sportsbooks` (`sportsbook_id`, `system_id`, `name`, `alias`) VALUES (%s,%s,%s,%s)"
-# cursor = cnx.cursor()
-# cursor.executemany(add_sportsbook_query, inserts)
+add_sportsbook_query = "INSERT INTO `sportsbooks` (`sportsbook_id`, `system_id`, `name`, `alias`) VALUES (%s,%s,%s,%s)"
+cursor = cnx.cursor()
+cursor.executemany(add_sportsbook_query, inserts)
+cnx.commit()
 cnx.close()
