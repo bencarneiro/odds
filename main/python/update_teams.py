6acc0824e6868df34df45e650a272e9ea1e621a7
Ben <ben@wizehire.com>
2021-10-24 19:13:01 -0500

    remove commented jupyter/pandas code

--- main/python/update_teams.py
+++ main/python/update_teams.py
@@ -18,12 +18,3 @@ cnx.commit()
 cnx.close()
 
 print(teams_inserts)
-
-
-# # Create Table of NFL Teams
-# nfl_teams = pd.DataFrame(columns=['sbr_abbreviation','abbreviation','team_id','name','nickname','location'],data=[])
-# for team in nfl.league_config()['teams']:
-#     team_row = pd.DataFrame(columns=['sbr_abbreviation','abbreviation','team_id','name','nickname','location'],data=[[team['sbr abbreviation'], team['abbreviation'],team['team id'],team['name'],team['nickname'],team['location']]])
-#     nfl_teams = nfl_teams.append(team_row, ignore_index=True)
-#     print(team)
-# print(nfl_teams)
